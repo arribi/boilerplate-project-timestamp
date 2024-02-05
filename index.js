ab52e8351ffccb4f409ab46602caf5a5aef37c7b
Jesús Arribi <arribi@boxer.local>
2024-02-05 10:38:30 +0000

    Timestamp Microservice

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,26 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  if (date) {
+    if (isNaN(date)) {
+      date = new Date(date);
+    } else {
+      date = new Date(parseInt(date));
+    }
+  } else {
+    date = new Date();
+  }
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" })
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
+  }
+})
 
 
 // listen for requests :)
